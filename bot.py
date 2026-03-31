4259744b0c9a2080e3e0f4d49e86ac60dd3e2db1
Numan Dokrul <91219439+numandokrul@users.noreply.github.com>
2026-03-31 13:45:46 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,10 +1,11 @@
 import requests
 import time
 import telebot
+import os
 
-TOKEN = "8534777848:AAH67sCjDpeLtlaJ4jVFvmpYbQ3vfz3nEOY"
-CHAT_ID = "877109239"
-API_KEY = "1d5b54b893mshb8c4507e1871b29p1d6e2fjsn5184b8543c1d"
+TOKEN = os.getenv("TOKEN")
+CHAT_ID = os.getenv("CHAT_ID")
+API_KEY = os.getenv("API_KEY")
 
 bot = telebot.TeleBot(TOKEN)
 bot.send_message(CHAT_ID, "TEST MESAJI")
@@ -61,13 +62,11 @@ def check():
         print("Fiyat:", price)
 
         if price:
-          
-          
 
-            if price_tl <= MAX_PRICE and price != last_sent:
+            if price <= MAX_PRICE and price != last_sent:
                 bot.send_message(
                     CHAT_ID,
-                    f"🔥 UCUZ UÇAK!\nSAW → Elazığ\n{int(price_tl)} TL"
+                    f"🔥 UCUZ UÇAK!\nSAW → Elazığ\n{int(price)} TL"
                 )
                 last_sent = price
 
